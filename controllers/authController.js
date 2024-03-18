9a164b3c296f628162b34ecfac5a6e54158f66b4
Samahsalah25 <Samahsalah2555@gmail.com>
2024-03-18 10:23:57 -0700

    edit image again

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,6 @@ signup = async (req, res, next) => {
       userName,
       Email,
       Password: hashedPassword,
-      Image: `https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png`,
     });
 
     console.log("User added successfully:", newUser);
