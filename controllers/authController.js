b650a59bc369958818af5cc6b803ec3b97c90b69
Samahsalah25 <Samahsalah2555@gmail.com>
2024-03-18 09:53:37 -0700

    add imag in registor by deafault`

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,7 @@ signup = async (req, res, next) => {
       userName,
       Email,
       Password: hashedPassword,
+      Image: `https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png`,
     });
 
     console.log("User added successfully:", newUser);
