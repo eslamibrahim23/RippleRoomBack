9a6c2f184c9bbe110fd3a2f038eed316723818ed
Samahsalah25 <Samahsalah2555@gmail.com>
2024-03-19 14:30:40 -0700

    update image attrubute

--- models/userSchema.js
+++ models/userSchema.js
@@ -22,10 +22,12 @@ const userSchema = new mongoose.Schema(
     },
     Address: {
       type: String,
+    },
+    Image: {
+      type: String,
       default:
         "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg",
     },
-
     Password: {
       type: String,
     },
